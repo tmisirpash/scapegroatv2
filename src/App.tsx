9d2b2eb98da2b89692e1292fc29a50b19c5dcfc1
tmisirpash <60826286+tmisirpash@users.noreply.github.com>
2022-11-25 23:29:29 -0800

    set up webpack, babel and react (#15)

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App: React.FC = () => <h1>Scapegroat</h1>;
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, './dist')
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|ts)x?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.jsx', '.js', '.tsx', '.ts'],
+    },
+}
