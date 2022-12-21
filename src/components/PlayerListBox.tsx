cf66e181d80778ae0d338f60c86cc750d32c059d
tmisirpash <60826286+tmisirpash@users.noreply.github.com>
2022-12-21 02:17:43 -0800

    change modal responsiveness (#31)

--- src/components/PlayerListBox.tsx
+++ src/components/PlayerListBox.tsx
@@ -1,43 +1,49 @@
 import React from 'react';
+import useMedia from '../hooks/useMedia';
 
 export interface playerListBox {
   addressQueue: string[];
   queuePtr: number;
+  width: string;
 }
 
 export function PlayerListBox(props: playerListBox) {
   const {
     addressQueue,
     queuePtr,
+    width,
   } = props;
+
+  const media = useMedia(700);
 
   return (
     <div
       style={{
         padding: '20px',
-        width: '50%',
+        width,
         float: 'right',
         fontSize: '1.5rem',
-        textAlign: 'left',
-        display: 'flex',
-        justifyContent: 'center',
+        boxSizing: 'border-box',
+        overflow: width === '100%' ? 'visible' : 'auto',
       }}
     >
       <table style={{
-        overflow: 'auto',
         width: '100%',
       }}
       >
         <thead>
-          <tr style={{ color: 'cyan' }}>
-            <th>Player Address</th>
-            <th>Position in Line</th>
+          <tr style={{
+            color: 'cyan',
+          }}
+          >
+            <th style={{ textAlign: 'center' }}>Player Address</th>
+            {media && <th>Position in Line</th>}
           </tr>
         </thead>
         <tbody>
           <tr>
             <td style={{ visibility: 'hidden' }}>0x</td>
-            <td style={{ visibility: 'hidden' }}>-1</td>
+            {media && <td style={{ visibility: 'hidden' }}>-1</td>}
           </tr>
           {addressQueue.map((a, i) => (
             <tr
@@ -47,9 +53,16 @@ export function PlayerListBox(props: playerListBox) {
                 borderBottomColor: '#303030',
               }}
             >
-              <td style={{ color: i < queuePtr ? 'white' : 'dimgray' }}>{a}</td>
-              {i < queuePtr && <td>{i + 1}</td>}
-              {i === queuePtr && <td style={{ fontSize: '0' }}>-1</td>}
+              <td
+                style={{
+                  color: i < queuePtr ? 'white' : 'dimgray',
+                  textAlign: 'center',
+                }}
+              >
+                {`${a.slice(0, 10)}...`}
+              </td>
+              {(media && i < queuePtr) && <td>{i + 1}</td>}
+              {(media && i === queuePtr) && <td style={{ fontSize: '0' }}>-1</td>}
             </tr>
           ))}
         </tbody>
--- src/hooks/useMedia.tsx
+++ src/hooks/useMedia.tsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react';
 
-export default function useMedia(): boolean {
+export default function useMedia(pixels: number): boolean {
   const [matches, setMatches] = useState(
-    window.matchMedia('(min-width: 768px)').matches,
+    window.matchMedia(`(min-width: ${pixels}px)`).matches,
   );
 
   useEffect(() => {
-    window.matchMedia('(min-width: 768px)').addEventListener('change', (e) => setMatches(e.matches));
+    window.matchMedia(`(min-width: ${pixels}px)`).addEventListener('change', (e) => setMatches(e.matches));
   }, []);
 
   return matches;
