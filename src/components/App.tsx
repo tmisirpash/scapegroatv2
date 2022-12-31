84a9ab79d42c1be7f7c90170705680a8a771497a
Timur Misirpashaev <tmisirpash@gmail.com>
2022-12-31 14:26:23 -0500

    add instructions

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -25,18 +25,59 @@ const App: React.FC = () => {
       <ConnectionStatus
         connectionStatusText={connectionStatusText}
       />
-      <div style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        paddingTop: '100px',
-        paddingBottom: '100px',
-      }}
-      >
-        <Table
-          chain={chain}
-          accountAddress={accountAddress}
-        />
+      <div>
+        <div style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          color: 'white',
+          fontSize: '3rem',
+          paddingTop: '50px',
+        }}
+        >
+          How to Play
+          <ol style={{
+            fontSize: 'min(2rem, 5vw)',
+            width: 'min(75%, 1000px)',
+          }}
+          >
+            <li>
+              Connect your wallet and choose a game to enter below.
+              The stake is the amount you need to gain a single entry.
+            </li>
+            <br />
+            <li>
+              You are free to add and remove entries as long as the game has
+              not been filled. Once all players have joined, the game is put
+              in a cooldown period.
+            </li>
+            <br />
+            <li>
+              After the cooldown period, one player is randomly chosen as
+              the &quot;scapegroat&quot;, and they lose the entirety of their stake.
+              All other players get their stake back in addition to an equal
+              share of the scapegroat&apos;s loss.
+            </li>
+            <br />
+            <li>
+              So, what are you waiting for? Don&apos;t be an antisocial antelope!
+            </li>
+          </ol>
+        </div>
+        <div style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          paddingTop: '100px',
+          paddingBottom: '100px',
+        }}
+        >
+          <Table
+            chain={chain}
+            accountAddress={accountAddress}
+          />
+        </div>
       </div>
     </div>
   );
