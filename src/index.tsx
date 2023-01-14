f932fe6fed1b0c3cbd6ca9568c47a5d6b40f7e76
Timur Misirpashaev <tmisirpash@gmail.com>
2023-01-13 20:49:28 -0500

    hacky fix for chrome wallet connection bug

--- src/index.tsx
+++ src/index.tsx
@@ -6,3 +6,15 @@ const rootNode = document.getElementById('app');
 if (rootNode) {
   createRoot(rootNode).render(<App />);
 }
+
+// Really not happy about this hack, but it will
+// do until the cause of the Chrome-on-Windows
+// bug becomes clearer.
+if (window.localStorage && !!window.chrome) {
+  if (!localStorage.getItem('scapeGroatFirstLoad')) {
+    localStorage.scapeGroatFirstLoad = true;
+    window.location.reload();
+  } else {
+    localStorage.removeItem('scapeGroatFirstLoad');
+  }
+}
