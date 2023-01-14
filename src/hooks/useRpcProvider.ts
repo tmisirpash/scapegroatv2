3e3b09b32b6f979b80ede4e3f1d020a54850cc60
Timur Misirpashaev <tmisirpash@gmail.com>
2023-01-13 23:24:24 -0500

    fix some wallet connection bugs

--- src/hooks/useRpcProvider.ts
+++ src/hooks/useRpcProvider.ts
@@ -3,13 +3,12 @@ import { ethers } from 'ethers';
 import { CHAIN_RPC_URLS, DEFAULT_CHAIN_ID } from '../utils/constants';
 
 export default function useRpcProvider(prov: any, chain: string, address: string) {
-
     const provider = useMemo(() => {
-        if (address === '0x' || !CHAIN_RPC_URLS.has(chain)) {
+        if (address === '0x' || !CHAIN_RPC_URLS.has(prov.chainId)) {
              return new ethers.providers.JsonRpcProvider(CHAIN_RPC_URLS.get(DEFAULT_CHAIN_ID));
         }
         return new ethers.providers.Web3Provider(prov);
-    }, [chain, address]);
+    }, [prov, chain, address]);
 
     return provider;
 }
