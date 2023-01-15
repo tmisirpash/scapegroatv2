dd71156f71f5251ea1963c103a0dcf79e3ab361c
Timur Misirpashaev <tmisirpash@gmail.com>
2023-01-15 02:32:37 -0500

    [trivial] change variable name

--- src/hooks/useRpcProvider.ts
+++ src/hooks/useRpcProvider.ts
@@ -5,7 +5,7 @@ import { CHAIN_RPC_URLS, DEFAULT_CHAIN_ID } from '../utils/constants';
 export default function useRpcProvider(prov: any, chain: string, address: string) {
     const providerChain = prov !== undefined && prov !== null ? prov.chainId : '';
     const provider = useMemo(() => {
-        if (address === '0x' || !CHAIN_RPC_URLS.has(prov.chainId)) {
+        if (address === '0x' || !CHAIN_RPC_URLS.has(providerChain)) {
                 return new ethers.providers.JsonRpcProvider(CHAIN_RPC_URLS.get(DEFAULT_CHAIN_ID));
         }
         return new ethers.providers.Web3Provider(prov);
