fdd7ad1f777ae614bd24e4c27117cec74214a930
Timur Misirpashaev <tmisirpash@gmail.com>
2023-01-15 00:49:58 -0500

    fix bug where if you user loads page on wrong network and switches to correct network, read-only provider is used

--- src/hooks/useRpcProvider.ts
+++ src/hooks/useRpcProvider.ts
@@ -3,12 +3,13 @@ import { ethers } from 'ethers';
 import { CHAIN_RPC_URLS, DEFAULT_CHAIN_ID } from '../utils/constants';
 
 export default function useRpcProvider(prov: any, chain: string, address: string) {
+    const providerChainId = prov !== undefined ? prov.chainId : '';
     const provider = useMemo(() => {
         if (address === '0x' || !CHAIN_RPC_URLS.has(prov.chainId)) {
              return new ethers.providers.JsonRpcProvider(CHAIN_RPC_URLS.get(DEFAULT_CHAIN_ID));
         }
         return new ethers.providers.Web3Provider(prov);
-    }, [prov, chain, address]);
+    }, [prov, chain, address, providerChainId]);
 
     return provider;
 }
